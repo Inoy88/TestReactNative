a1300b6980f9fe43ebd4496edf30d17e41cc1516
Konstantin Sharovarov <inoy@Mac-mini-Konstantin.local>
2023-10-03 20:33:08 +0300

    - Edit Navigator

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -66,7 +66,7 @@ const AppStack = observer(function AppStack() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false, navigationBarColor: colors.background }}
-      initialRouteName={isAuthenticated ? "Welcome" : "Login"} // @demo remove-current-line
+      initialRouteName={isAuthenticated ? "Demo" : "Login"} // @demo remove-current-line
     >
       {/* @demo remove-block-start */}
       {isAuthenticated ? (
